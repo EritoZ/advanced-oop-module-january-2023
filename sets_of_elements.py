bdfd1db67f9dfb50181086d2c86a4cf8fb4cd90e
EritoZ <moni.grozdev@gmail.com>
2023-01-15 21:47:35 +0200

    Delete sets_of_elements.py

--- sets_of_elements.py
+++
@@ -1,7 +0,0 @@
-n_el_first_set, n_el_second_set = input().split()
-first_set = {input() for n in range(int(n_el_first_set))}
-second_set = {input() for num in range(int(n_el_second_set))}
-
-diff = first_set.intersection(second_set)
-
-print('\n'.join(diff))
