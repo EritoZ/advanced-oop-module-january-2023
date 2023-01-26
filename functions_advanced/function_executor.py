6a3b6fc160dc04157aba11f15506927b6574610c
EritoZ <moni.grozdev@gmail.com>
2023-01-26 12:52:20 +0200

    Update function_executor.py

--- functions_advanced/function_executor.py
+++ functions_advanced/function_executor.py
@@ -3,28 +3,3 @@ def func_executor(*args):
     func_results = [f'{func.__name__} - {func(*parameters)}' for func, parameters in args]
 
     return '\n'.join(func_results)
-
-
-def sum_numbers(num1, num2):
-    return num1 + num2
-
-def multiply_numbers(num1, num2):
-    return num1 * num2
-
-print(func_executor(
-    (sum_numbers, (1, 2)),
-    (multiply_numbers, (2, 4))
-))
-
-def make_upper(*strings):
-    result = tuple(s.upper() for s in strings)
-    return result
-
-def make_lower(*strings):
-    result = tuple(s.lower() for s in strings)
-    return result
-
-print(func_executor(
-    (make_upper, ("Python", "softUni")),
-    (make_lower, ("PyThOn",)),
-))
