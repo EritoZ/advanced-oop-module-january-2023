3c6902c4be4cf61e4a295b0414332c913a4d91df
EritoZ <moni.grozdev@gmail.com>
2023-01-26 12:50:34 +0200

    Update math_operations.py

--- functions_advanced/math_operations.py
+++ functions_advanced/math_operations.py
@@ -9,26 +9,22 @@ def math_operations(*args: float, **kwargs):
     while float_nums:
 
         for letter in letters_dict:
+            num = float_nums.popleft()
 
             if letter == 'a':
-                letters_dict[letter] += float_nums.popleft()
+                letters_dict[letter] += num
 
             elif letter == 's':
-                letters_dict[letter] -= float_nums.popleft()
+                letters_dict[letter] -= num
 
             elif letter == 'd':
-                num = float_nums.popleft()
-
                 if num != 0:
                     letters_dict[letter] /= num
 
             elif letter == 'm':
-                letters_dict[letter] *= float_nums.popleft()
+                letters_dict[letter] *= num
 
             if not float_nums:
                 break
 
     return '\n'.join(f'{key}: {value:.1f}' for key, value in sorted(letters_dict.items(), key=lambda x: (-x[1], x[0])))
-
-
-print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7, d=33, m=15))
