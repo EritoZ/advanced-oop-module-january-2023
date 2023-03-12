ceda40f07b915872db5b1d58061f7ecb8aafa18d
EritoZ <moni.grozdev@gmail.com>
2023-03-12 21:30:57 +0200

    Update mercedes_team.py

--- polymorphism_and_abstraction/formula_1_manager/project/formula_teams/mercedes_team.py
+++ polymorphism_and_abstraction/formula_1_manager/project/formula_teams/mercedes_team.py
@@ -3,13 +3,8 @@ from project.formula_teams.formula_team import FormulaTeam
 
 class MercedesTeam(FormulaTeam):
 
-    def calculate_revenue_after_race(self, race_pos: int):
-        sponsor_rewards = ({1: 1000000, 3: 500000}, {5: 100000, 7: 50000})
+    def team_income(self):
+        sponsors = ({1: 1000000, 3: 500000}, {5: 100000, 7: 50000})
+        costs = -200000
 
-        reward = self.check_winnings(sponsor_rewards, race_pos)
-
-        reward -= 200000
-
-        self.budget += reward
-
-        return self.status_message(reward, self.budget)
+        return sponsors, costs
