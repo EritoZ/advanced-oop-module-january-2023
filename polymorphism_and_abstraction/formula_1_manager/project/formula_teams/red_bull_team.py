d83760484bb02006e8ccb8500137cf7dae0de679
EritoZ <moni.grozdev@gmail.com>
2023-03-12 21:31:13 +0200

    Update red_bull_team.py

--- polymorphism_and_abstraction/formula_1_manager/project/formula_teams/red_bull_team.py
+++ polymorphism_and_abstraction/formula_1_manager/project/formula_teams/red_bull_team.py
@@ -3,13 +3,9 @@ from project.formula_teams.formula_team import FormulaTeam
 
 class RedBullTeam(FormulaTeam):
 
-    def calculate_revenue_after_race(self, race_pos: int):
-        sponsor_rewards = ({1: 1500000, 2: 800000}, {8: 20000, 10: 10000})
+    def team_income(self):
+        sponsors = ({1: 1500000, 2: 800000}, {8: 20000, 10: 10000})
 
-        reward = self.check_winnings(sponsor_rewards, race_pos)
+        costs = -250000
 
-        reward -= 250000
-
-        self.budget += reward
-
-        return self.status_message(reward, self.budget)
+        return sponsors, costs
